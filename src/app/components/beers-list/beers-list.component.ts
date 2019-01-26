b4108a025531030a9f9a91f5bb4cbab457411a87
Michal Skora <mchlskora@gmail.com>
2019-01-26 17:07:07 +0100

    Selected brewer persisted in localStorage

--- src/app/components/beers-list/beers-list.component.ts
+++ src/app/components/beers-list/beers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 import { BeersService } from 'src/app/services/beers.service';
 import { Observable } from 'rxjs';
 import { BrewerService } from 'src/app/services/brewer.service';
@@ -13,18 +13,26 @@ export class BeersListComponent implements OnInit {
   public brewers$: Observable<string[]>;
   public beers: Beer[] = [];
   public pending = false;
+  public selected: string;
+
+  @Input() name: string;
 
   constructor(private brewerService: BrewerService, private beerService: BeersService) {}
 
   public ngOnInit() {
+    this.selected = localStorage.getItem(this.keyName) || '';
+
     this.brewers$ = this.brewerService.brewers$;
+
+    this.getBeers(this.selected);
   }
 
-  onChange({ value }) {
-    this.pending = true;
-    this.beers = [];
+  get keyName() {
+    return `column-${this.name}`;
+  }
 
-    this.beerService.getBeers(value).subscribe(response => {
+  private getBeers(brewer: string) {
+    this.beerService.getBeers(brewer).subscribe(response => {
       this.pending = false;
       this.beers = response.sort((a, b) => {
         if (a.name > b.name) {
@@ -38,5 +46,14 @@ export class BeersListComponent implements OnInit {
         return 0;
       });
     });
+  }
+
+  onChange({ value }) {
+    this.pending = true;
+    this.beers = [];
+
+    localStorage.setItem(this.keyName, value);
+
+    this.getBeers(value);
   }
 }
